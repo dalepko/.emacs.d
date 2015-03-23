000a7444a1c4539cfe15bcd629932552fbc560b6
David Couderc <dacouderc@yahoo.fr>
2015-03-23 10:28:58 +0100

    some config changes

--- init.el
+++ init.el
@@ -8,6 +8,9 @@
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(backup-by-copying t)
  '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backup"))))
+ '(circe-default-nick "dcouderc")
+ '(circe-network-options (quote (("Freenode" :nickserv-password "Iè'Nvh*ù$" :nick "dcouderc"))))
+ '(clean-buffer-list-delay-general 1)
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(delete-old-versions t)
@@ -15,9 +18,12 @@
  '(global-git-gutter-mode t)
  '(ido-enable-flex-matching t)
  '(ido-everywhere t)
+ '(ido-ignore-files (quote ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" ".orig$")))
  '(ido-mode (quote both) nil (ido))
  '(indent-tabs-mode nil)
+ '(kept-new-versions 10)
  '(menu-bar-mode nil)
+ '(midnight-mode t nil (midnight))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
@@ -26,18 +32,21 @@
  '(rm-blacklist (quote (" hl-p" " AC" " GitGutter" " MRev" " $")))
  '(safe-local-variable-values
    (quote
-    ((project-venv-name . "tina-develop")
+    ((project-venv-name . "tina-2.2")
+     (project-venv-name . "tina-develop")
      (project-venv-name . "netlink2"))))
  '(scss-compile-at-save nil)
- '(sgml-basic-offset 4)
- '(show-paren-mode t))
+ '(sgml-basic-offset 2)
+ '(show-paren-mode t)
+ '(vc-make-backup-files t)
+ '(version-control t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(flymake-errline ((t (:background "black"))) t)
- '(flymake-warnline ((t (:background "black"))) t)
+ '(flymake-errline ((t (:background "black"))))
+ '(flymake-warnline ((t (:background "black"))))
  '(git-gutter:added ((t (:foreground "color-40" :weight bold))))
  '(match ((t (:background "magenta")))))
 
