8ce57a579f3a839df659951db8f13d943a478605
David Couderc <dacouderc@yahoo.fr>
2016-09-17 17:09:02 +0200

    moved to company and flycheck

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-(package-initialize)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -20,11 +18,13 @@
  '(custom-enabled-themes (quote (misterioso)))
  '(custom-safe-themes
    (quote
-    ("66132890ee1f884b4f8e901f0c61c5ed078809626a547dbefbb201f900d03fd8" "3632cf223c62cb7da121be0ed641a2243f7ec0130178722554e613c9ab3131de" "7e83d0aacca4c0e4e9441f920a66ee4de73decc1bb9dd7fcc2c1857948e604c8" "a444b2e10bedc64e4c7f312a737271f9a2f2542c67caa13b04d525196562bf38" "e8a9dfa28c7c3ae126152210e3ccc3707eedae55bdc4b6d3e1bb3a85dfb4e670" "de8fa309eed1effea412533ca5d68ed33770bdf570dcaa458ec21eab219821fd" "8abee8a14e028101f90a2d314f1b03bed1cde7fd3f1eb945ada6ffc15b1d7d65" "9cb6358979981949d1ae9da907a5d38fb6cde1776e8956a1db150925f2dad6c1" "5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" "3a9249d4c34f75776e130efd7e02c4a0a7c90ad7723b50acc5806112394ec2dd" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "f3d6a49e3f4491373028eda655231ec371d79d6d2a628f08d5aa38739340540b" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "66132890ee1f884b4f8e901f0c61c5ed078809626a547dbefbb201f900d03fd8" "3632cf223c62cb7da121be0ed641a2243f7ec0130178722554e613c9ab3131de" "7e83d0aacca4c0e4e9441f920a66ee4de73decc1bb9dd7fcc2c1857948e604c8" "a444b2e10bedc64e4c7f312a737271f9a2f2542c67caa13b04d525196562bf38" "e8a9dfa28c7c3ae126152210e3ccc3707eedae55bdc4b6d3e1bb3a85dfb4e670" "de8fa309eed1effea412533ca5d68ed33770bdf570dcaa458ec21eab219821fd" "8abee8a14e028101f90a2d314f1b03bed1cde7fd3f1eb945ada6ffc15b1d7d65" "9cb6358979981949d1ae9da907a5d38fb6cde1776e8956a1db150925f2dad6c1" "5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" "3a9249d4c34f75776e130efd7e02c4a0a7c90ad7723b50acc5806112394ec2dd" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(delete-old-versions t)
  '(ediff-split-window-function (quote split-window-horizontally))
  '(eshell-cmpl-cycle-completions nil)
+ '(fci-rule-color "#383838")
  '(flycheck-display-errors-delay 0.5)
+ '(flycheck-python-pylint-executable "~/.emacs.d/epylint")
  '(foreground-color "#839496")
  '(frame-resize-pixelwise t)
  '(global-git-gutter-mode t)
@@ -45,13 +45,11 @@
     ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "\\.orig$" "\\.bak$")))
  '(ido-mode (quote both) nil (ido))
  '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
  '(kept-new-versions 10)
  '(menu-bar-mode nil)
  '(midnight-mode t nil (midnight))
- '(package-archives
    (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(rich-minority-mode t)
  '(rm-blacklist
    (quote
@@ -65,6 +63,7 @@
  '(scroll-bar-mode nil)
  '(scss-compile-at-save nil)
  '(sgml-basic-offset 4)
+ '(shell-file-name "/bin/bash")
  '(shell-pop-universal-key "C-p")
  '(show-paren-mode t)
  '(tool-bar-mode nil)
@@ -82,6 +81,9 @@
  '(git-gutter:added ((t (:foreground "color-40" :weight bold))))
  '(match ((t (:background "magenta"))))
  '(web-mode-html-attr-name-face ((t (:foreground "green")))))
+
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
 
 (global-set-key (kbd "M-<up>") '(lambda () (interactive) (scroll-other-window -1)))
 (global-set-key (kbd "M-<down>") '(lambda () (interactive) (scroll-other-window 1)))
@@ -103,19 +105,19 @@
 
 (add-hook 'web-mode-hook (lambda () (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
-(defun my-flymake-show-help ()
-  (when (get-char-property (point) 'flymake-overlay)
-    (let ((help (get-char-property (point) 'help-echo)))
-      (if help (message "%s" help)))))
+;; (defun my-flymake-show-help ()
+;;   (when (get-char-property (point) 'flymake-overlay)
+;;     (let ((help (get-char-property (point) 'help-echo)))
+;;       (if help (message "%s" help)))))
 
-(when (load "flymake" t)
-  (defun flymake-erlang-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name temp-file
-                                           (file-name-directory buffer-file-name))))
-      (list "~/.emacs.d/eerlc" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks '("\\.erl\\'" flymake-erlang-init)))
+;; (when (load "flymake" t)
+;;   (defun flymake-erlang-init ()
+;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                        'flymake-create-temp-inplace))
+;;            (local-file (file-relative-name temp-file
+;;                                            (file-name-directory buffer-file-name))))
+;;       (list "~/.emacs.d/eerlc" (list local-file))))
+;;   (add-to-list 'flymake-allowed-file-name-masks '("\\.erl\\'" flymake-erlang-init)))
 
 (defun my-erlang-setup ()
   (add-to-list 'write-file-functions 'delete-trailing-whitespace)
@@ -130,8 +132,8 @@
 (global-set-key [(f9)] 'compile)
 (global-set-key [(f1)] 'man)
 
-(require 'auto-complete-config)
-(ac-config-default)
+;;(require 'auto-complete-config)
+;;(ac-config-default)
 
 
 ;;--[yas configuration]--------------------------------------------------
@@ -162,18 +164,17 @@
 ;; (define-key yas-minor-mode-map (kbd "TAB") nil)
 ;; (define-key yas-minor-mode-map (kbd "C-*") 'yas-expand)
 
+
 ;;--[haskell-mode configuration]------------------------------------------
 
-;; (require 'flycheck)
-;; (flycheck-disable-checker 'haskell-ghc)
-;; (flycheck-disable-checker 'haskell-stack-ghc)
-;; (add-hook 'haskell-mode-hook 'flycheck-mode)
-;; (add-hook 'flycheck-mode-hook #'flycheck-haskell-setup)
-
-(load-file "~/.emacs.d/hspec.el")
-
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
-
+(add-hook 'haskell-mode-hook
+          (lambda ()
+            (require 'company)
+            (set (make-local-variable 'company-backends)
+                 (append '((company-capf company-dabbrev-code))
+                         company-backends))
+            (company-mode)))
 
 (add-hook 'interactive-haskell-mode-hook
           (lambda ()
@@ -181,6 +182,18 @@
             (define-key interactive-haskell-mode-map (kbd "C-c C-t") 'haskell-mode-show-type-at)
             (define-key interactive-haskell-mode-map [(shift f9)] 'hspec-run)
             (define-key interactive-haskell-mode-map [(f9)] 'hspec-rerun)))
+
+;; (add-hook 'haskell-mode-hook 'intero-mode)
+
+(load-file "~/.emacs.d/hspec.el")
+
+(add-hook 'interactive-haskell-mode-hook
+          (lambda ()
+            (define-key interactive-haskell-mode-map [(shift f9)] 'hspec-run)
+            (define-key interactive-haskell-mode-map [(f9)] 'hspec-rerun)))
+
+
+;;------------------------------------------------------------------------
 
 (setq exec-path (append exec-path '("~/.local/bin")))
 
--- python-setup.el
+++ python-setup.el
@@ -1,120 +1,102 @@
+;;; -*- lexical-binding: t -*-
+
+(require 'company)
+(require 'company-jedi)
+(require 'gud)
 (require 'python)
+(require 'which-func)
 
-(when (load "which-func")
-  (setq pytest-last-file nil)
-  (setq pytest-last-func nil)
+(defvar pytest-last-file nil)
+(defvar pytest-last-func nil)
 
-  (defvar pytest-error-minor-mode-map
-    (let ((map (make-sparse-keymap)))
-      (define-key map [mouse-2] 'compile-goto-error)
-      (define-key map [follow-link] 'mouse-face)
-      (define-key map "\C-c\C-c" 'compile-goto-error)
-      (define-key map "\C-c\C-k" 'kill-compilation)
-      (define-key map "\M-n" 'compilation-next-error)
-      (define-key map "\M-p" 'compilation-previous-error)
-      (define-key map "\M-{" 'compilation-previous-file)
-      (define-key map "\M-}" 'compilation-next-file)
-      map)
-    "Keymap for `pytest-error-minor-mode-map'.")
+(defvar pytest-error-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [mouse-2] 'compile-goto-error)
+    (define-key map [follow-link] 'mouse-face)
+    (define-key map "\C-c\C-c" 'compile-goto-error)
+    (define-key map "\C-c\C-k" 'kill-compilation)
+    (define-key map "\M-n" 'compilation-next-error)
+    (define-key map "\M-p" 'compilation-previous-error)
+    (define-key map "\M-{" 'compilation-previous-file)
+    (define-key map "\M-}" 'compilation-next-file)
+    map)
+  "Keymap for `pytest-error-minor-mode-map'.")
 
-  (define-minor-mode pytest-error-minor-mode
-    "Highlight errors in pytest buffer"
-    nil " Pytest"
-    :group 'compilation
-    (if pytest-error-minor-mode
-        (compilation-setup t)
-      (compilation--unsetup)))
+(define-minor-mode pytest-error-minor-mode
+  "Highlight errors in pytest buffer"
+  nil " Pytest"
+  :group 'compilation
+  (if pytest-error-minor-mode
+      (compilation-setup t)
+    (compilation--unsetup)))
 
-  (defun undedicate-window (window old-sentinel proc msg)
-    (cond ((and (windowp window) (memq (process-status proc) '(signal exit)))
-           (set-window-dedicated-p window nil)))
-    (funcall old-sentinel proc msg))
+(defun undedicate-window (window old-sentinel proc msg)
+  (cond ((and (windowp window) (memq (process-status proc) '(signal exit)))
+         (set-window-dedicated-p window nil)))
+  (funcall old-sentinel proc msg))
 
-  (defun run-pytest (verbose filename func)
-    (let ((command  (format "py.test%s -v"
-                            ;;(file-name-directory filename)
-                            (if verbose " -s --pdb" ""))))
-      (if func
-          (let ((node_id (concat filename;;(file-name-nondirectory filename)
-                                 "::"
-                                 (mapconcat 'identity (split-string func "\\.") "::"))))
-            (setq command (concat command " " node_id)))
-        (setq command (concat command " " (file-name-nondirectory filename))))
+(defun run-pytest (verbose filename func)
+  (let ((command  (format "py.test%s -v"
+                          ;;(file-name-directory filename)
+                          (if verbose " -s --pdb" ""))))
+    (if func
+        (let ((node_id (concat filename;;(file-name-nondirectory filename)
+                               "::"
+                               (mapconcat 'identity (split-string func "\\.") "::"))))
+          (setq command (concat command " " node_id)))
+      (setq command (concat command " " (file-name-nondirectory filename))))
 
-      (setq pytest-last-file filename)
-      (setq pytest-last-func func)
-      (let ((buffer (save-window-excursion
-                      (when (and (boundp 'gud-comint-buffer) (buffer-live-p gud-comint-buffer))
-                        (set-buffer gud-comint-buffer)
-                        (erase-buffer))
-                      (pdb command)
-                      (setq gud-find-expr-function (lambda () (symbol-name (sexp-at-point))))
-                      (pytest-error-minor-mode)
-                      (current-buffer))))
-        (save-selected-window
-          (switch-to-buffer-other-window buffer)
-          (let* ((process (get-buffer-process buffer))
-                 (old-sentinel (process-sentinel process)))
-            (set-window-dedicated-p nil t)
-            (set-process-sentinel process
-                                  `(lambda (proc msg)
-                                     (undedicate-window ,(selected-window)
-                                                        (quote ,old-sentinel)
-                                                        proc msg)))
-            )))))
-      ;(compilation-start command t '(lambda (mode) "*py.test*"))))
-
-  (defun pytest (&optional verbose)
-    (interactive "P")
-    (run-pytest verbose buffer-file-name (which-function)))
-
-  (defun pytest-again (&optional verbose)
-    (interactive "P")
-    (if pytest-last-file
-        (run-pytest verbose pytest-last-file pytest-last-func)
-      (run-pytest verbose buffer-file-name (which-function))))
-
-  (define-key python-mode-map [(shift f9)] 'pytest)
-  (define-key python-mode-map [(f9)] 'pytest-again))
+    (setq pytest-last-file filename)
+    (setq pytest-last-func func)
+    (let ((buffer (save-window-excursion
+                    (when (and (boundp 'gud-comint-buffer) (buffer-live-p gud-comint-buffer))
+                      (set-buffer gud-comint-buffer)
+                      (erase-buffer))
+                    (pdb command)
+                    (setq gud-find-expr-function (lambda () (symbol-name (sexp-at-point))))
+                    (pytest-error-minor-mode)
+                    (current-buffer))))
+      (save-selected-window
+        (switch-to-buffer-other-window buffer)
+        (let* ((process (get-buffer-process buffer))
+               (old-sentinel (process-sentinel process)))
+          (set-window-dedicated-p nil t)
+          (set-process-sentinel process
+                                `(lambda (proc msg)
+                                   (undedicate-window ,(selected-window)
+                                                      (quote ,old-sentinel)
+                                                      proc msg)))
+          )))))
 
 
-(when (load "flymake" t)
-  (setq epylint "~/.emacs.d/epylint")
+(defun pytest (&optional verbose)
+  (interactive "P")
+  (run-pytest verbose buffer-file-name (which-function)))
 
-  (defun flymake-pylint-init ()
-    (interactive)
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-		       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-
-      (setq epylint-filename local-file)
-      (list epylint (list local-file))))
-
-  (add-to-list 'flymake-allowed-file-name-masks
-	       '("\\.py\\'" flymake-pylint-init)))
+(defun pytest-again (&optional verbose)
+  (interactive "P")
+  (if pytest-last-file
+      (run-pytest verbose pytest-last-file pytest-last-func)
+    (run-pytest verbose buffer-file-name (which-function))))
 
 
-(defun my-flymake-show-help ()
-  (when (get-char-property (point) 'flymake-overlay)
-    (let ((help (get-char-property (point) 'help-echo)))
-      (if help (message "%s" help)))))
-
-
-(setq venv-indicator '(:exec venv-current-name))
-
+(defconst venv-indicator '(:exec venv-current-name))
 
 (defun my-python-setup ()
-  (require 'virtualenvwrapper)
   (if (not (eq (car mode-line-format) venv-indicator))
       (setq mode-line-format (cons venv-indicator mode-line-format)))
-  (car mode-line-format)
+
+  (add-to-list 'company-backends #'company-jedi)
+
   (hack-local-variables)
+  (define-key python-mode-map [(shift f9)] 'pytest)
+  (define-key python-mode-map [(f9)] 'pytest-again)
   (define-key python-mode-map (kbd "RET") 'newline-and-indent)
+
   (add-to-list 'write-file-functions 'delete-trailing-whitespace)
-  (add-hook 'post-command-hook 'my-flymake-show-help)
+
   (when (boundp 'project-venv-name)
     (venv-workon project-venv-name))
+  (company-mode)
   (jedi:setup)
-  (flymake-mode t))
+  (flycheck-mode t))
